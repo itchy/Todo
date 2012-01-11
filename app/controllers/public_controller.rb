7c50dba09f95a4357f722d57624715e47ee4b5a3
Scott Johnson <7.scott.j@gmail.com>
2012-01-11 11:25:50 -0600

    have text create new todo item

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -13,10 +13,17 @@ class PublicController < ApplicationController
     body = params[:Body]
     from = params[:From]
     from_zip =  params[:FromZip]
-    if from && body && # account_sid == TWILIO_ACCOUNT_SID 
-      sms = TwilioNet.new()
-      sms.send_sms('2146680255', truncate("FROM:#{from}  BODY:#{body}", :length => 159) )
-      render :layout => 'empty', :template => 'public/sms'  
+    if from && body && # account_sid == TWILIO_ACCOUNT_SID
+      # sms = TwilioNet.new()
+      # sms.send_sms('2146680255', truncate("FROM:#{from}  BODY:#{body}", :length => 159) )
+      # render :layout => 'empty', :template => 'public/sms'  
+      creator = User.find_or_create_by_sms_number(from)
+      creator.create_task(body)
+      unless creator.valid?
+        sms = TwilioNet.new()
+        sms.send_sms(from, truncate(creator.message, :length => 159) )
+      end
+      render :text => "Task #{body} created."  
     else
       raise error  
     end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   
   
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-  validates :email, :uniqueness => true 
+  # validates :email, :uniqueness => true 
   validates :sms_number, :format => { :with => /\A[(]*([0-9]){3}[)-\. ]*([0-9]{3})[-\.]*([0-9]{4})\Z/i, :on => :create, :message => "Phone Number is invalid"}
   validates :password, :presence => true, :on => :create
   
@@ -13,9 +13,24 @@ class User < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
   
+  def create_task(body)
+    if body[/^\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z$/i]
+      # if the total content of the text is an email address, update the users email address
+      self.email = body
+      self.save
+    else
+      task = tasks.new({:status => "Open", :description => body}) 
+      task.save 
+    end
+  end
+  
   class << self
     def select_user
       self.all.map {|u| [u.display_name, u.id] }
+    end
+    
+    def find_or_create_by_sms_number(number)
+      creator = self.find_by_sms_number(number) || User.new({:sms_number => number, :password => 'leclerk', :email => "unknown@sample.com"}) 
     end
   end  
 end
